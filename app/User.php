4f5896a0e261630d03288af9536b0107f4ce77aa
unknown <joasdeveloper@gmail.com>
2019-05-30 02:51:15 -0500

    Likes

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email','role','imgPort','imgProf'
+        'name','password', 'email','role','imgPort','imgProf'
     ];
 
     /**
